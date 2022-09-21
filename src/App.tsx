68e3511e22dc53f5a12b5f752a6265aebfb31dec
Alex Maystrenko <maystrenko_alex@mail.ru>
2022-09-22 02:54:48 +0300

    less20 - activeClass at NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 import { Dialogs } from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Music from './components/Music/Music';
-import Navbar from './components/Navbar/Navbar';
+import { Navbar } from './components/Navbar/Navbar';
 import News from './components/News/News';
 import Profile from './components/Profile/Profile';
 import Settings from './components/Settings/Settings';
@@ -12,21 +12,21 @@ import Settings from './components/Settings/Settings';
 function App() {
   return (
     <BrowserRouter>
-    <div className="App_wrapper">
-      <Header />
-      <div className='wrapper'>
-        <Navbar />
-        <div className='content'>
-          <Routes>
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/dialogs' element={<Dialogs />} />
-            <Route path='/news' element={<News />} />
-            <Route path='/music' element={<Music />} />
-            <Route path='/settings' element={<Settings />} />
-          </Routes>
+      <div className="App_wrapper">
+        <Header />
+        <div className='wrapper'>
+          <Navbar />
+          <div className='content'>
+            <Routes>
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/dialogs' element={<Dialogs />} />
+              <Route path='/news' element={<News />} />
+              <Route path='/music' element={<Music />} />
+              <Route path='/settings' element={<Settings />} />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
     </BrowserRouter>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,15 +3,35 @@ import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
 
 export const Navbar = () => {
+
   return (
     <nav className={s.navbar}>
-          <div className={s.item}><NavLink to='/profile'>Profile</NavLink></div> 
-          <div className={s.item}><NavLink to='/dialogs'>Message</NavLink></div>
-          <div className={s.item}><NavLink to='/news'>News</NavLink></div>
-          <div className={s.item}><NavLink to='/music'>Music</NavLink></div>
-          <div className={s.item}><NavLink to='/settings'>Settings</NavLink></div>
-        </nav>
+      <div className={s.item}>
+        <NavLink to='/profile' className={({ isActive }) => isActive ? s.activeClass : ''} >
+          Profile
+        </NavLink>
+      </div>
+
+      <div className={s.item}>
+        <NavLink to='/dialogs' className={({ isActive }) => isActive ? s.activeClass : ''}>
+          Message
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to='/news' className={({ isActive }) => isActive ? s.activeClass : ''}>
+          News
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to='/music' className={({ isActive }) => isActive ? s.activeClass : ''}>
+          Music
+        </NavLink>
+      </div>
+      <div className={s.item}
+      ><NavLink to='/settings' className={({ isActive }) => isActive ? s.activeClass : ''}>
+          Settings
+        </NavLink>
+      </div>
+    </nav>
   );
 };
-
-export default Navbar;
