f6e1eed39a7df82194fede605015a75497fa947f
Alex Maystrenko <maystrenko_alex@mail.ru>
2022-09-21 23:52:11 +0300

    less18 - layout Dialogs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Dialogs } from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
@@ -10,7 +11,10 @@ function App() {
       <Header />
       <div className='wrapper'>
         <Navbar />
-        <Profile />
+        <div className='content'>
+          {/* <Profile /> */}
+          <Dialogs />
+        </div>
       </div>
     </div>
   );
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,15 +4,15 @@ import s from './Profile.module.css';
 
 export const Profile = () => {
   return (
-    <div className={s.content}>
-          <div className={s.content_images}>
-            <img alt='' src='https://st.depositphotos.com/1927453/2592/i/600/depositphotos_25922381-stock-photo-sunset-over-green-wheat-field.jpg'></img>
-          </div>
-          <div>
-            ava+description
-          </div>
-          <MyPosts />
-        </div>
+    <div >
+      <div className={s.content_images}>
+        <img alt='' src='https://st.depositphotos.com/1927453/2592/i/600/depositphotos_25922381-stock-photo-sunset-over-green-wheat-field.jpg'></img>
+      </div>
+      <div>
+        ava+description
+      </div>
+      <MyPosts />
+    </div>
   );
 };
 
