0893b6ce2cb3ad6c7ef9bd69be5a5c4a04b6cb24
Alex Maystrenko <maystrenko_alex@mail.ru>
2022-09-22 03:36:36 +0300

    less21 - layout Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css';
 
 export const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>
+          Dimych
+        </div>
+        <div className={ s.dialog}>
+          Andrew
+        </div>
+        <div className={s.dialog}>
+          Sveta
+        </div>
+        <div className={s.dialog}>
+          Sasha
+        </div>
+        <div className={s.dialog}>
+          Viktor
+        </div>
+        <div className={s.dialog}>
+          Valera
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.dialog}>Hi</div>  
+        <div className={s.dialog}>How are you?</div>  
+        <div className={s.dialog}>Yooo</div>  
+      </div>
     </div>
   );
 };
