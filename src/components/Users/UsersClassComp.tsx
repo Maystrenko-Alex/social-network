45d5b9bf33f8e17a40171864dcb04de72660edba
Alex Maystrenko <maystrenko_alex@mail.ru>
2022-11-23 22:49:12 +0300

    less55 - componentDidMount

--- src/components/Users/UsersClassComp.tsx
+++ src/components/Users/UsersClassComp.tsx
@@ -13,18 +13,31 @@ type UserPropsType = {
 }
 
 class Users extends React.Component<UserPropsType> {
-  constructor(props:UserPropsType) {
-    super(props);
-    if (this.props.users.length === 0) {
-      axios.get(' https://social-network.samuraijs.com/api/1.0/users').then(response => {
+  // constructor(props:UserPropsType) {
+  //   super(props);
+  //   if (this.props.users.length === 0) {
+  //     axios.get(' https://social-network.samuraijs.com/api/1.0/users').then(response => {
+  //       this.props.setUsers(response.data.items)
+  //     })
+  //   }  
+  // }
+componentDidMount(): void {
+  if (this.props.users.length === 0) {
+  axios.get(' https://social-network.samuraijs.com/api/1.0/users').then(response => {
         this.props.setUsers(response.data.items)
       })
-    }  
-  }
-
+    }
+}
   render() {
     return (
       <div>
+        <div className={s.pagesRow}>
+          <span>1</span>
+          <span className={s.selectedPages}>2</span>
+          <span>3</span>
+          <span>4</span>
+          <span>5</span>
+        </div>
         {/* <button onClick={this.getUsers}>GET USERS</button> */}
         {
           this.props.users.map(user => {
